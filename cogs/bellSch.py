1ca0169ba3bcb5d8b5c6ab2a52b36ed2332078a4
ajinoxx <57328840+ajinoxx@users.noreply.github.com>
2020-09-28 14:02:51 -0400

    Update bellSch.py

--- cogs/bellSch.py
+++ cogs/bellSch.py
@@ -62,108 +62,110 @@ class bellSch(commands.Cog):
 
         print(f'isaHoliday = {isaHoliday} with the date of {currentDate}')
 
-        if dayOfWeek == 5 or dayOfWeek == 6 or isaHoliday:#lambda d: d == currentDate, holidays:
-            self.bellSch.cancel()
-            print('The loop was canceled.')
-        print('The loop was not canceled.')
-        print("It's a day of the week")
-        print(f"It's {currentSec} into the minute")
+        #if dayOfWeek == 5 or dayOfWeek == 6 or isaHoliday:#lambda d: d == currentDate, holidays:
+        #    self.bellSch.cancel()
+        #    print('The loop was canceled.')
+        
+        if dayOfWeek != 5 and dayOfWeek != 6 or not isaHoliday:
+            print('The loop was not canceled.')
+            print("It's a day of the week")
+            print(f"It's {currentSec} into the minute")
 
-        """ if currentTime == '07:23':
-            print('Period 1 starts in 5 minutes!')
-            await tChannel.send(f'{tRole.mention} Period 1 starts in 5 minutes!')
-        elif currentTime == '07:28':
-            print('Period 1 has started!')
-            await tChannel.send(f'{tRole.mention} Period 1 has started!')
-        elif currentTime == '08:03':
-            print('Period 1 ends in 5 minutes!')
-            await tChannel.send(f'{tRole.mention} Period 1 ends in 5 minutes!')
-        elif currentTime == '08:08':
-            print('Period 1 has ended!')
-            await tChannel.send(f'{tRole.mention} Period 1 has ended!')
-        
-        elif currentTime == '08:12':
-            print('Period 2 starts in 5 minutes!')
-            await tChannel.send(f'{tRole.mention} Period 2 starts in 5 minutes!')
-        elif currentTime == '08:17':
-            print('Period 2 has started!')
-            await tChannel.send(f'{tRole.mention} Period 2 has started!')
-        elif currentTime == '08:52':
-            print('Period 2 ends in 5 minutes!')
-            await tChannel.send(f'{tRole.mention} Period 2 ends in 5 minutes!')
-        elif currentTime == '08:57':
-            print('Period 2 has ended!')
-            await tChannel.send(f'{tRole.mention} Period 2 has ended!')
-        
-        elif currentTime == '09:01':
-            print('Period 3 has started!')
-            await tChannel.send(f'{tRole.mention} Period 3 has started!')
-        elif currentTime == '09:36':
-            print('Period 3 ends in 5 minutes!')
-            await tChannel.send(f'{tRole.mention} Period 3 ends in 5 minutes!')
-        elif currentTime == '09:41':
-            print('Period 3 has ended!')
-            await tChannel.send(f'{tRole.mention} Period 3 has ended!')            
-        
-        elif currentTime == '09:45':
-            print('Period 4 has started!')
-            await tChannel.send(f'{tRole.mention} Period 4 has started!')
-        elif currentTime == '10:20':
-            print('Period 4 ends in 5 minutes!')
-            await tChannel.send(f'{tRole.mention} Period 4 ends in 5 minutes!')
-        elif currentTime == '10:25':
-            print('Period 4 has ended!')
-            await tChannel.send(f'{tRole.mention} Period 4 has ended!')
-        
-        elif currentTime == '10:29':
-            print('Period 5 has started!')
-            await tChannel.send(f'{tRole.mention} Period 5 has started!')
-        elif currentTime == '11:04':
-            print('Period 5 ends in 5 minutes!')
-            await tChannel.send(f'{tRole.mention} Period 5 ends in 5 minutes!')
-        elif currentTime == '11:09':
-            print('Period 5 has ended!')
-            await tChannel.send(f'{tRole.mention} Period 5 has ended!')
-        
-        elif currentTime == '11:13':
-            print('Period 6 has started!')
-            await tChannel.send(f'{tRole.mention} Period 6 has started!')
-        elif currentTime == '11:48':
-            print('Period 6 ends in 5 minutes!')
-            await tChannel.send(f'{tRole.mention} Period 6 ends in 5 minutes!')
-        elif currentTime == '11:53':
-            print('Period 6 has ended!')
-            await tChannel.send(f'{tRole.mention} Period 6 has ended!')
+            if currentTime == '07:23':
+                print('Period 1 starts in 5 minutes!')
+                await tChannel.send(f'{tRole.mention} Period 1 starts in 5 minutes!')
+            elif currentTime == '07:28':
+                print('Period 1 has started!')
+                await tChannel.send(f'{tRole.mention} Period 1 has started!')
+            elif currentTime == '08:03':
+                print('Period 1 ends in 5 minutes!')
+                await tChannel.send(f'{tRole.mention} Period 1 ends in 5 minutes!')
+            elif currentTime == '08:08':
+                print('Period 1 has ended!')
+                await tChannel.send(f'{tRole.mention} Period 1 has ended!')
+            
+            elif currentTime == '08:12':
+                print('Period 2 starts in 5 minutes!')
+                await tChannel.send(f'{tRole.mention} Period 2 starts in 5 minutes!')
+            elif currentTime == '08:17':
+                print('Period 2 has started!')
+                await tChannel.send(f'{tRole.mention} Period 2 has started!')
+            elif currentTime == '08:52':
+                print('Period 2 ends in 5 minutes!')
+                await tChannel.send(f'{tRole.mention} Period 2 ends in 5 minutes!')
+            elif currentTime == '08:57':
+                print('Period 2 has ended!')
+                await tChannel.send(f'{tRole.mention} Period 2 has ended!')
+            
+            elif currentTime == '09:01':
+                print('Period 3 has started!')
+                await tChannel.send(f'{tRole.mention} Period 3 has started!')
+            elif currentTime == '09:36':
+                print('Period 3 ends in 5 minutes!')
+                await tChannel.send(f'{tRole.mention} Period 3 ends in 5 minutes!')
+            elif currentTime == '09:41':
+                print('Period 3 has ended!')
+                await tChannel.send(f'{tRole.mention} Period 3 has ended!')            
+            
+            elif currentTime == '09:45':
+                print('Period 4 has started!')
+                await tChannel.send(f'{tRole.mention} Period 4 has started!')
+            elif currentTime == '10:20':
+                print('Period 4 ends in 5 minutes!')
+                await tChannel.send(f'{tRole.mention} Period 4 ends in 5 minutes!')
+            elif currentTime == '10:25':
+                print('Period 4 has ended!')
+                await tChannel.send(f'{tRole.mention} Period 4 has ended!')
+            
+            elif currentTime == '10:29':
+                print('Period 5 has started!')
+                await tChannel.send(f'{tRole.mention} Period 5 has started!')
+            elif currentTime == '11:04':
+                print('Period 5 ends in 5 minutes!')
+                await tChannel.send(f'{tRole.mention} Period 5 ends in 5 minutes!')
+            elif currentTime == '11:09':
+                print('Period 5 has ended!')
+                await tChannel.send(f'{tRole.mention} Period 5 has ended!')
+            
+            elif currentTime == '11:13':
+                print('Period 6 has started!')
+                await tChannel.send(f'{tRole.mention} Period 6 has started!')
+            elif currentTime == '11:48':
+                print('Period 6 ends in 5 minutes!')
+                await tChannel.send(f'{tRole.mention} Period 6 ends in 5 minutes!')
+            elif currentTime == '11:53':
+                print('Period 6 has ended!')
+                await tChannel.send(f'{tRole.mention} Period 6 has ended!')
 
-        elif currentTime == '11:57':
-            print('Period 7 has started!')
-            await tChannel.send(f'{tRole.mention} Period 7 has started!')
-        elif currentTime == '12:32':
-            print('Period 7 ends in 5 minutes!')
-            await tChannel.send(f'{tRole.mention} Period 7 ends in 5 minutes!')
-        elif currentTime == '12:37':
-            print('Period 7 has ended!')
-            await tChannel.send(f'{tRole.mention} Period 7 has ended!')
+            elif currentTime == '11:57':
+                print('Period 7 has started!')
+                await tChannel.send(f'{tRole.mention} Period 7 has started!')
+            elif currentTime == '12:32':
+                print('Period 7 ends in 5 minutes!')
+                await tChannel.send(f'{tRole.mention} Period 7 ends in 5 minutes!')
+            elif currentTime == '12:37':
+                print('Period 7 has ended!')
+                await tChannel.send(f'{tRole.mention} Period 7 has ended!')
 
-        elif currentTime == '12:41':
-            print('Period 8 has started!')
-            await tChannel.send(f'{tRole.mention} Period 8 has started!')
-        elif currentTime == '13:16':
-            print('Period 8 ends in 5 minutes!')
-            await tChannel.send(f'{tRole.mention} Period 8 ends in 5 minutes!')
-        elif currentTime == '13:21':
-            print('Period 8 has ended!')
-            await tChannel.send(f'{tRole.mention} Period 8 has ended!')
+            elif currentTime == '12:41':
+                print('Period 8 has started!')
+                await tChannel.send(f'{tRole.mention} Period 8 has started!')
+            elif currentTime == '13:16':
+                print('Period 8 ends in 5 minutes!')
+                await tChannel.send(f'{tRole.mention} Period 8 ends in 5 minutes!')
+            elif currentTime == '13:21':
+                print('Period 8 has ended!')
+                await tChannel.send(f'{tRole.mention} Period 8 has ended!')
 
-        elif currentTime == '13:25':
-            print('Period 9 has started!')
-            await tChannel.send(f'{tRole.mention} Period 9 has started!')
-        elif currentTime == '14:00':
-            print('Period 9 ends in 5 minutes!')
-            await tChannel.send(f'{tRole.mention} Period 9 ends in 5 minutes!')
-        elif currentTime == '14:05':
-            print('Period 9 has ended!')
-            await tChannel.send(f'{tRole.mention} Period 9 has ended!') """
+            elif currentTime == '13:25':
+                print('Period 9 has started!')
+                await tChannel.send(f'{tRole.mention} Period 9 has started!')
+            elif currentTime == '14:00':
+                print('Period 9 ends in 5 minutes!')
+                await tChannel.send(f'{tRole.mention} Period 9 ends in 5 minutes!')
+            elif currentTime == '14:05':
+                print('Period 9 has ended!')
+                await tChannel.send(f'{tRole.mention} Period 9 has ended!')
             
     @bellSch.before_loop
     async def before_bellSch(self):
