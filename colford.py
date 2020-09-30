a4c3af60a32bb474891fb973c480a141e87963a7
ajinoxx <57328840+ajinoxx@users.noreply.github.com>
2020-09-30 13:20:17 -0400

    Update colford.py

--- colford.py
+++ colford.py
@@ -9,8 +9,17 @@ import os
 
 client = commands.Bot(command_prefix = '.')
 
+scienceClubID = 760895586659467334
+biggusBrainusID = 543185885232103434
+
 logsID = 645817145007144981 #755893521478582409 #test
-mainID = 753429614465908817 #754438628758913098 #test
+mainBBID = 753429614465908817 #754438628758913098 #test
+
+mainSCID = 760895587184541777
+starterSCID = 760896390955401216
+boardID = 760895783015809084
+logsSCID = 760911595143561276
+muteSCID = 760912284154724452
 
 adminID = 627267659364302848 #754449876401520846 #test
 starterID = 543187460021288960 #754449876401520846 #test
@@ -35,17 +44,25 @@ async def on_ready():
 @client.event
 async def on_member_join(member):
     print(f'{member} joined the server.')
-    mainChannel = client.get_channel(mainID)
+    #member.guild gives the guild
+    #mainChannel = client.get_channel(mainBBID)
+    mainChannel = member.guild.get_channel(mainBBID)
+    if mainChannel == None:
+        mainChannel = member.guild.get_channel(mainSCID)
     await mainChannel.send(f'{member.mention} ({member}) joined the server.')
     #await member.dm_channel.send("Welcome to Biggus Brainus!\nIf you'd like to get notified on class bell updates, make you sure you check out the roles channel.")
     if member.id != trooperID:
-        role = discord.utils.get(member.guild.roles, id=starterID) #Make sure IrvinsMom role is ABOVE the target role
+        role = member.guild.get_role(starterID) #discord.utils.get(member.guild.roles, id=starterID) #Make sure IrvinsMom role is ABOVE the target role
+        if role == None:
+            role = member.guild.get_role(starterSCID)
         await member.add_roles(role)
 
 @client.event
 async def on_member_remove(member):
     print(f'{member} left the server.') 
-    mainChannel = client.get_channel(mainID)
+    mainChannel = member.guild.get_channel(mainBBID)
+    if mainChannel == None:
+        mainChannel = member.guild.get_channel(mainSCID)
     await mainChannel.send(f'{member.mention} ({member}) left the server.')
 
 @client.event
@@ -91,6 +108,12 @@ async def clear(ctx, amount=5):
             numMessage = await ctx.send(f'Cleared {amount} messages.')
             await ctx.guild.get_channel(logsID).send(f'{ctx.author.mention} ({ctx.author}) cleared {amount} messages in {ctx.channel.mention} ({ctx.channel}).')
             await numMessage.delete(delay=3)
+        elif discord.utils.find(lambda r: r.id == boardID, ctx.message.author.roles):
+            print(f'{ctx.message.author} has admin role, clearing {amount} messages in {ctx.channel}.')    
+            await ctx.channel.purge(limit=amount)
+            numMessage = await ctx.send(f'Cleared {amount} messages.')
+            await ctx.guild.get_channel(logsSCID).send(f'{ctx.author.mention} ({ctx.author}) cleared {amount} messages in {ctx.channel.mention} ({ctx.channel}).')
+            await numMessage.delete(delay=3)
         else:
             await ctx.send('You don\'t have the permissions to do that')
     except:
@@ -111,6 +134,13 @@ async def clearUser(ctx, tag : discord.abc.User, amount=10):
             await ctx.guild.get_channel(logsID).send(f'{ctx.author.mention} ({ctx.author}) searched {amount} messages, cleared {len(deleted)} messages from {tag.mention} ({tag}) in {ctx.channel.mention} ({ctx.channel}).')
             await numMessage.delete(delay=5)
             await ctx.message.delete(delay=None)
+        elif discord.utils.find(lambda r: r.id == boardID, ctx.message.author.roles):                       
+            deleted = await ctx.channel.purge(limit=amount, check=is_user)
+            print(f'{ctx.message.author} has admin role, searching {amount} messages from {tag.name}. Cleared {len(deleted)} messages in {ctx.channel}.')
+            numMessage = await ctx.send(f'Searched {amount} messages, cleared {len(deleted)} messages from {tag.name}.')
+            await ctx.guild.get_channel(logsSCID).send(f'{ctx.author.mention} ({ctx.author}) searched {amount} messages, cleared {len(deleted)} messages from {tag.mention} ({tag}) in {ctx.channel.mention} ({ctx.channel}).')
+            await numMessage.delete(delay=5)
+            await ctx.message.delete(delay=None)
         else:
             await ctx.send('You don\'t have the permissions to do that.')
     except:
@@ -125,6 +155,13 @@ async def mute(ctx, tag : discord.Member):
         await tag.remove_roles(starterRole)
         await ctx.guild.get_channel(logsID).send(f'{ctx.author.mention} ({ctx.author}) muted {tag.mention} ({tag.name})')
         await ctx.send(f'Muted {tag.mention}')
+    elif discord.utils.find(lambda r: r.id == boardID, ctx.message.author.roles):
+        muteRole = discord.utils.get(ctx.guild.roles, id=muteSCID)
+        starterRole = discord.utils.get(ctx.guild.roles, id=starterSCID)
+        await tag.add_roles(muteRole)
+        await tag.remove_roles(starterRole)
+        await ctx.guild.get_channel(logsSCID).send(f'{ctx.author.mention} ({ctx.author}) muted {tag.mention} ({tag.name})')
+        await ctx.send(f'Muted {tag.mention}')
     else:
         await ctx.send("You don't have permission to use this command.")
 
@@ -137,13 +174,20 @@ async def unmute(ctx, tag : discord.Member):
         await tag.add_roles(starterRole)
         await ctx.guild.get_channel(logsID).send(f'{ctx.author.mention} ({ctx.author}) unmuted {tag.mention} ({tag.name})')
         await ctx.send(f'Unmuted {tag.mention}')
+    elif discord.utils.find(lambda r: r.id == boardID, ctx.message.author.roles):
+        muteRole = discord.utils.get(ctx.guild.roles, id=muteSCID)
+        starterRole = discord.utils.get(ctx.guild.roles, id=starterSCID)
+        await tag.remove_roles(muteRole)
+        await tag.add_roles(starterRole)
+        await ctx.guild.get_channel(logsSCID).send(f'{ctx.author.mention} ({ctx.author}) unmuted {tag.mention} ({tag.name})')
+        await ctx.send(f'Unmuted {tag.mention}')
     else:
         await ctx.send("You don't have permission to use this command.")
 
 @client.command(aliases=['v'])
 async def version(ctx):
     if ctx.author.id == myID:
-        await ctx.send('Bot version 1.5.0')
+        await ctx.send('Bot version 1.6.0')
 
 @client.command()
 async def dow(ctx):
